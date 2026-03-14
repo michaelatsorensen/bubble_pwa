f5473f32cb44e5d3eb3bf7e27390671570e9ebd5
michaelatsorensen <michaelatsorensen@gmail.com>
2026-03-14 19:27:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // ══════════════════════════════════════
 // Version managed by CACHE_NAME below
 
-const CACHE_NAME = 'bubble-v3.9.2';
+const CACHE_NAME = 'bubble-v3.9.3';
 const CACHE_URLS = [
   './', './index.html', './app.css',
   './bubble-icons.js', './tag-data.js',
